8414a83bdd1c2a785cf14258ee1a289601bfa02e
Natasha Medina <32655689+natashasmg@users.noreply.github.com>
2022-03-26 16:51:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 function compute()
 {
-		var principal = document.getElementById("principal").value;
-    
-    if(principal < 0 || principal == 0){
-  		alert ("Enter a positive number, please");
+	var principal = document.getElementById("principal").value;
+	if(principal < 0 || principal == 0){
+		alert ("Enter a positive number, please");
  }
- 		else {
- 			var rate = document.getElementById("rate").value;
-    	var years = document.getElementById("years").value;
-      var interest = principal * years * rate /100;
-      var year = new Date().getFullYear()+parseInt(years);
-      document.getElementById("result").innerHTML="\<br\>If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+	else {
+		var rate = document.getElementById("rate").value;
+    		var years = document.getElementById("years").value;
+      		var interest = principal * years * rate /100;
+      		var year = new Date().getFullYear()+parseInt(years);
+      	document.getElementById("result").innerHTML="\<br\>If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
 }
 }
 
 function updateRate() 
 {
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    	var rateval = document.getElementById("rate").value;
+   	document.getElementById("rate_val").innerText=rateval;
 }
         
